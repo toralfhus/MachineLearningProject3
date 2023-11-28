1db58052528743a79a0b6889b544e0341e65ec68
toralf <toralf95@gmail.com>
2023-11-28 20:27:01 +0100

    Added simple image loader using pydicom

---
+++ src/utils.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import sys
+
+
+DIR_MAIN_IMAGES = r"C:\rsna-miccai-brain-tumor-radiogenomic-classification"
+
+
+def load_images(train=True, image_mode="FLAIR"):
+    import pydicom
+
+    dir = os.path.join(DIR_MAIN_IMAGES, "train" if train else "test")
+    print(dir)
+    patients = os.listdir(dir)
+    print("Having", len(patients), "train" if train else "test", "patients:", patients)
+    for pt in patients:
+
+        print(f"LOADING PT{pt} {image_mode}", end="\t")
+        path = os.path.join(dir, pt, image_mode)
+
+        # Sort images by name (verify dicom positioning?)
+        images = [int(nm.split("-")[-1].split(".")[0]) for nm in os.listdir(path)]
+        images.sort()
+        images = [f"Image-{i}.dcm" for i in images]
+
+
+        voxels = []
+
+        for im in images:
+            ds = pydicom.dcmread(os.path.join(path, im))
+            arr = ds.pixel_array
+            voxels.append(arr)
+        voxels = np.array(voxels)
+        print(voxels.shape)
+
+        yield voxels
+
+    pass
+
+
+if __name__ == "__main__":
+    img = next(load_images(train=True))
+    from matplotlib import pyplot as plt
+    plt.imshow(img[img.shape[0] // 2])
+    plt.show()
